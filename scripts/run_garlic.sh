8c3deafdec77770f7dfe09d9b13855acc31a7ed4
Martin <martin.adam.stoffel@gmail.com>
2020-06-05 12:43:47 +0100

    remove low call rate ids

--- scripts/run_garlic.sh
+++ scripts/run_garlic.sh
@@ -1,13 +1,13 @@
 #!/bin/sh	
 # Grid engine options
-#$ -N transpose_ped
+#$ -N run_garlic
 #$ -wd /exports/csce/eddie/biology/groups/pemberton/martin/sheep_roh/
-#$ -l h_rt=03:00:00
+#$ -l h_rt=00:30:00
 #$ -M martin.adam.stoffel@gmail.com
 #$ -m beas
-#$ -l h_vmem=60G
+#$ -l h_vmem=30G
 #$ -o o_files/
 #$ -e e_files/
 
 
-/exports/csce/eddie/biology/groups/pemberton/martin/utils/bin/garlic/bin/linux/garlic --tped data/sheep_geno_trans.tped --tfam data/sheep_geno_trans.tfam --centromere data/dummy_cent --error 0.005 --winsize 60 --out output/roh 
+/exports/csce/eddie/biology/groups/pemberton/martin/utils/bin/garlic/bin/linux/garlic --tped data/sheep_geno_trans.tped --tfam data/sheep_geno_trans.tfam --centromere data/dummy_cent --error 0.005 --winsize 100 --overlap-frac 0.5 --out output/roh 
